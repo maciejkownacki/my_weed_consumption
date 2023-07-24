675303a119e3d2225259fef7542d4ce2aaf1dc68
Maciej Kownacki <97414660+maciejkownacki@users.noreply.github.com>
2023-07-24 19:20:25 +0200

    v. 0.2

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(
-      const Duration(seconds: 3),
+      const Duration(seconds: 2),
           () => Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => MyHomePage()),
